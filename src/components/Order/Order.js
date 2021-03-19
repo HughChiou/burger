840442718a3d2f09dd381cb0f151e2100bb3ac94
Hugh Chiou <hughchiou@gmail.com>
2021-03-19 09:40:22 +0000

    update styles

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import classes from './Order.module.css';
 import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider';
 
 const Order = (props) => {
-  console.log(props);
   return (
     <div className={classes.Order}>
       <h3>Order Id: {props.id}</h3>
+      <Divider variant="middle" />
       {[...Object.entries(props.ingredients)].map((ing) => (
         <Chip
           key={ing[0]}
@@ -16,7 +17,7 @@ const Order = (props) => {
           color="secondary"
           variant="outlined"
           avatar={<Avatar>{ing[1]}</Avatar>}
-          style={{ textTransform: 'capitalize', margin: '0 3px' }}></Chip>
+          style={{ textTransform: 'capitalize', margin: '20px 3px 0' }}></Chip>
       ))}
       <p>
         Price: <strong>USD {props.price.toFixed(2)}</strong>
